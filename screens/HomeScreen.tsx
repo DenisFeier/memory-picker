3e2587a49ebd5a100cbd2ef1d91060bc8998e5df
Denis Feier <denisfeier@Deniss-MacBook-Pro.local>
2025-05-02 12:14:18 +0300

    make it work with api calls

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,27 +4,34 @@ import { StatusBar } from 'expo-status-bar';
 import axios from 'axios';
 import ScreenWrapper from '../components/ScreenWrapper';
 
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJQTE0iLCJlbWFpbCI6InRlc3QxQG1haWxpbmF0b3IuY29tIiwiaWF0IjoxNzQ2MTc2NTc2LCJleHAiOjE3NDYyMTk3NzZ9.zgvpZ9HoCDzT0iOk30K71Iud0Jz4z53F3pTbrAuYOWw"
+
 export default function HomeScreen() {
   const [posts, setPosts] = useState([]);
   const [numColumns] = useState(2);
 
   useEffect(() => {
-    axios.get('http://192.168.63.104:3000/api/post/12?page=1&limit=3&order=desc', {
-      headers: {
-        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIsInVzZXJuYW1lIjoib3JpY2VlIiwiZW1haWwiOiJvcmljZWVAbWFpbGluYXRvci5jb20iLCJpYXQiOjE3NDYxNjg4NTQsImV4cCI6MTc0NjIxMjA1NH0.3xf5wUTjQ2vh-14zwiJZsxpfeA9xIuEWwXyCjxoLuUU',
-      },                
-    })
-    .then(response => {
-      setPosts(response.data);
-    })
-    .catch(error => {
-      console.error('Error fetching posts:', error);
-    });
-  }, []);
+    const fetchPosts = async () => {
+      axios.get('http://127.0.0.1:3000/api/post/12?page=1&limit=3&order=desc', {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        }                
+      })
+      .then(response => {
+        console.log(response.data)
+        setPosts(response.data.posts);
+      })
+      .catch(error => {
+        console.error(JSON.stringify(error));
+      });
+    }
+    fetchPosts()
+  }, [setPosts]);
 
   const renderItem = ({ item }) => (
+    console.log(item.picture),
     <TouchableOpacity style={styles.imageContainer} onPress={() => console.log('To the post', item.id)}>
-      <Image source={{ uri: item.imageUrl }} style={styles.image} />
+      <Image source={{ uri: item.picture }} style={styles.image} />
     </TouchableOpacity>
   );
 
@@ -33,7 +40,7 @@ export default function HomeScreen() {
       <FlatList
         data={posts}
         renderItem={renderItem}
-        keyExtractor={(item) => item.id()}
+        keyExtractor={(item) => item.id}
         numColumns={2}
         key={numColumns}
         contentContainerStyle={styles.list}
